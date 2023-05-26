c30f177d526cfb487b58d89e7128cfa7214baae2
3121kz <zelinsky.erik17@gmail.com>
2023-05-26 09:13:35 +0200

    Commit after prettier

--- todo/todo/src/App.js
+++ todo/todo/src/App.js
@@ -4,18 +4,20 @@ import { useState } from "react";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [todo,setTodo] = useState("");
+  const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
   return (
     <div className="App">
       <Header> </Header>
-      <Form 
-      todo={todo} 
-      setTodo={setTodo} 
-      todoList={todoList} 
-      setTodoList={setTodoList}> 
-      </Form>
-      <TodoList setTodoList={setTodoList} todoList={todoList}> </TodoList>
+      <Form
+        todo={todo}
+        setTodo={setTodo}
+        todoList={todoList}
+        setTodoList={setTodoList}
+      ></Form>
+      <TodoList setTodoList={setTodoList} todoList={todoList}>
+        {" "}
+      </TodoList>
     </div>
   );
 }
